3ef93ef10db5597141cd8eb11f7108cd94a77920
rooneyish <ilustraterp@gmail.com>
2025-01-19 10:17:46 +0545

    Revert "Revert "Minor Fixes""
    This reverts commit 3f178007f896c5596e8cbf59e9d09217a5555f8d.

--- GameSpace/src/com/gamespace/controller/algorithms/CustomInsertionSort.java
+++ GameSpace/src/com/gamespace/controller/algorithms/CustomInsertionSort.java
@@ -11,6 +11,7 @@ import java.util.List;
  */
 public class CustomInsertionSort {
     private List<GameModel> gameSortedList;
+    
     public CustomInsertionSort(){
         gameSortedList = new ArrayList<>();
     }
