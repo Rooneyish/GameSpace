a84fef0dfa919ea33b353b1c2993ebce9521b34d
rooneyish <ilustraterp@gmail.com>
2025-01-19 10:16:52 +0545

    Minor Fixes

--- GameSpace/src/com/gamespace/controller/algorithms/CustomBinarySearch.java
+++ GameSpace/src/com/gamespace/controller/algorithms/CustomBinarySearch.java
@@ -1,4 +1,3 @@
-
 package com.gamespace.controller.algorithms;
 
 import com.gamespace.model.GameModel;
@@ -9,7 +8,7 @@ import java.util.List;
  * @author Ronish Prajapati
  */
 public class CustomBinarySearch {
-        public GameModel searchByName(String searchValue, List<GameModel> gameList,
+    public GameModel searchByName(String searchValue, List<GameModel> gameList,
             int left, int right) {
 
         // Base Case
@@ -21,7 +20,7 @@ public class CustomBinarySearch {
         int mid = (left + right) / 2;
 
         // checks whether searchKey lies on mid point
-        if (searchValue.compareToIgnoreCase(gameList.get(mid).getGameName()) == 0) {      
+        if (searchValue.compareToIgnoreCase(gameList.get(mid).getGameName()) == 0) {
             return gameList.get(mid);
         } else if (searchValue.compareToIgnoreCase(gameList.get(mid).getGameName()) < 0) {
             return searchByName(searchValue, gameList, left, mid - 1);
